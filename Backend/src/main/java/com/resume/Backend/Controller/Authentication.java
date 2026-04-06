62bbf788619916c7b467d2a6387057d98f41d37f
Manikanta-Konga <manikantakonga0210@gmail.com>
2026-04-06 17:39:57 +0530

    Added and configured h2 database

--- Backend/src/main/java/com/resume/Backend/Controller/Authentication.java
+++ Backend/src/main/java/com/resume/Backend/Controller/Authentication.java
@@ -1,4 +1,12 @@
 package com.resume.Backend.Controller;
 
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class Authentication {
+
+
+
 }
--- Backend/src/main/java/com/resume/Backend/Model/Resume.java
+++ Backend/src/main/java/com/resume/Backend/Model/Resume.java
@@ -1,16 +1,22 @@
 package com.resume.Backend.Model;
 
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
 import lombok.*;
 
 import java.time.LocalDate;
 
 @Data
+@Entity
 @Setter
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
 public class Resume {
+
+    @Id
+    private Long id;
 
     private String fileType;
     private String content;
