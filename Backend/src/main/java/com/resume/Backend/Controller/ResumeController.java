693a5b424101962023edb7af6e8639a494833381
Manikanta-Konga <manikantakonga0210@gmail.com>
2026-04-09 10:21:29 +0530

    Started Uploading file, but not completed yet

--- Backend/src/main/java/com/resume/Backend/Controller/ResumeController.java
+++ Backend/src/main/java/com/resume/Backend/Controller/ResumeController.java
@@ -1,4 +1,28 @@
 package com.resume.Backend.Controller;
 
+import com.resume.Backend.Service.ResumeService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+@RestController
+@RequestMapping("/api")
 public class ResumeController {
+
+    private final ResumeService service;
+
+    ResumeController(ResumeService service) {
+        this.service = service;
+    }
+
+
+    @PostMapping("/upload")
+    public ResponseEntity<String> upoloadResume(@RequestBody MultipartFile resFile) {
+        return ResponseEntity.status(200).body(service.uploadResume(resFile));
+    }
+
+
 }
---
+++ Backend/src/main/java/com/resume/Backend/Service/ResumeService.java
@@ -0,0 +1,36 @@
+package com.resume.Backend.Service;
+
+import com.resume.Backend.Repository.ResumeRepo;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+@Service
+public class ResumeService {
+
+    private final ResumeRepo repo;
+
+    ResumeService(ResumeRepo repo) {
+        this.repo = repo;
+    }
+
+    public String uploadResume(MultipartFile resFile) {
+        String fileName = resFile.getOriginalFilename();
+        long size = resFile.getSize();
+        double fileSize = (double) size / (1024 * 1024);
+        if(fileSize <= 5) {
+            throw new RuntimeException("File size too large");
+        }
+        if((fileName.endsWith(".pdf") || fileName.endsWith(".docx"))) {
+            if(resFile.isEmpty()) {
+                throw new RuntimeException("File is Empty, again upload resume");
+            }
+        } else {
+            throw new RuntimeException("File type is not supported");
+        }
+
+        return "Resume File is Uploaded Successfully";
+    }
+
+
+
+}
